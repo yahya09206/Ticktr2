7108128805f5170680d9de4a3930fc7e7415bd16
Yahya <yahyahussein91@gmail.com>
2023-09-29 21:49:12 -0700

    added comments

--- src/main/java/com/cydeo/TicketingProjectMVC.java
+++ src/main/java/com/cydeo/TicketingProjectMVC.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TicketingProjectMVC {
 
     // Method to start up server
-    // Time to get started
+    // Time to get started tomorrow night
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectMVC.class, args);
     }
