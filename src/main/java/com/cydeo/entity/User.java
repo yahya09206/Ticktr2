abf2d036a2ec85da52c9aa81aa90c9ad19ddb583
Yahya <yahyahussein91@gmail.com>
2023-10-01 14:45:59 -0700

    setting up pojo for user

---
+++ src/main/java/com/cydeo/entity/User.java
@@ -0,0 +1,15 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Gender;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private boolean enabled;
+    private String phone;
+    private Role role;
+    private Gender gender;
+}
