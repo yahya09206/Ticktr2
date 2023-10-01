af071dfd098ffbb8fde7fa9e577e369b5100adc4
Yahya <yahyahussein91@gmail.com>
2023-10-01 15:34:06 -0700

    extending crud service to user

--- src/main/java/com/cydeo/service/UserService.java
+++ src/main/java/com/cydeo/service/UserService.java
@@ -4,12 +4,6 @@ import com.cydeo.dto.UserDTO;
 
 import java.util.List;
 
-public interface UserService {
-
-    UserDTO save(UserDTO user);
-    UserDTO findById(String username);
-    List<UserDTO> findAll();
-    void delete(UserDTO user);
-    void deleteById(UserDTO username);
+public interface UserService extends CrudService<UserDTO, String>{
 
 }
