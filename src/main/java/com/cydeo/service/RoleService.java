94a5f3e0a20ecb389207729836a0c59322d6141c
Yahya <yahyahussein91@gmail.com>
2023-10-01 15:28:58 -0700

    created user service

--- src/main/java/com/cydeo/service/RoleService.java
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -1,6 +1,17 @@
 package com.cydeo.service;
 
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+
+import java.util.List;
+
 public interface RoleService {
 
     // save, findBy, delete, findAll
+
+    RoleDTO save(RoleDTO role);
+    RoleDTO findByID(Long id);
+    List<RoleDTO> findAll();
+    void delete(RoleDTO role);
+    void deleteById(Long id);
 }
---
+++ src/main/java/com/cydeo/service/UserService.java
@@ -0,0 +1,15 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.UserDTO;
+
+import java.util.List;
+
+public interface UserService {
+
+    UserDTO save(UserDTO user);
+    UserDTO findById(String username);
+    List<UserDTO> findAll();
+    void delete(UserDTO user);
+    void deleteById(UserDTO username);
+
+}
