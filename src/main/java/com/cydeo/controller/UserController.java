fe7977d601d5f9afa17da985f0ce11e4ecfa75c1
Yahya <yahyahussein91@gmail.com>
2023-10-01 14:30:16 -0700

    setting up user controller

---
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user/create")
+public class UserController {
+
+
+}
