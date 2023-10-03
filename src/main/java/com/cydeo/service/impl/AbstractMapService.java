9a2bc63a28dd60dd7b786a7cf18d130ba7ad7ff6
Yahya <yahyahussein91@gmail.com>
2023-10-02 21:27:02 -0700

    generic method for object call

--- src/main/java/com/cydeo/service/impl/AbstractMapService.java
+++ src/main/java/com/cydeo/service/impl/AbstractMapService.java
@@ -6,4 +6,10 @@ import java.util.Map;
 public class AbstractMapService<T, ID> {
 
     protected Map<ID, T> map = new HashMap<>();
+
+    T save(ID id, T object){
+
+        map.put(id, object);
+        return object;
+    }
 }
