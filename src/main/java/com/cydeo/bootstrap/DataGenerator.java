bd81b9a1ce0ea61847c28cd401629f48c132e2cf
Yahya <yahyahussein91@gmail.com>
2023-10-02 21:55:47 -0700

    implemented commandline runner for roles

--- src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,6 +1,15 @@
 package com.cydeo.bootstrap;
 
-public class DataGenerator {
+import com.cydeo.dto.RoleDTO;
+import org.springframework.boot.CommandLineRunner;
 
+public class DataGenerator implements CommandLineRunner {
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        RoleDTO adminRole = new RoleDTO(1L, "Admin");
+        RoleDTO managerRole = new RoleDTO(2L, "Manager");
+        RoleDTO employeeRole = new RoleDTO(3L, "Employee");
+    }
 }
