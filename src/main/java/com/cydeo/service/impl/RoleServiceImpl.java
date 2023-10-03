daae28b6172f4f21f97f452ba24af790fdb4c0fd
Yahya <yahyahussein91@gmail.com>
2023-10-02 21:32:31 -0700

    implementing role service

---
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -0,0 +1,35 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.CrudService;
+import com.cydeo.service.RoleService;
+
+import java.util.List;
+
+public class RoleServiceImpl extends AbstractMapService<RoleDTO, Long> implements RoleService {
+
+    @Override
+    public RoleDTO save(RoleDTO object) {
+        return null;
+    }
+
+    @Override
+    public List<T> findAll() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(ID id) {
+
+    }
+
+    @Override
+    public RoleDTO findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public void delete(RoleDTO object) {
+
+    }
+}
