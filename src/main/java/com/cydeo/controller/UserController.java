658c595ba9ccf4f70a41f7b0c9561c9167b22a1a
Yahya <yahyahussein91@gmail.com>
2023-10-03 20:58:09 -0700

    added user service to user controller

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -25,6 +25,7 @@ public class UserController {
 
         model.addAttribute("user", new UserDTO());
         model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
         return "/user/create";
     }
 }
