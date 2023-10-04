059f1bce40a45fbaed240da5896d1f54693c418b
Yahya <yahyahussein91@gmail.com>
2023-10-03 21:50:49 -0700

    done for night

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -32,7 +32,7 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    public String insertUser(UserDTO user, Model model){
+    public String insertUser(@ModelAttribute("user") UserDTO user, Model model){
 
         model.addAttribute("user", new UserDTO());
         model.addAttribute("roles", roleService.findAll());
